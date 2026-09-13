fbed6a81a283894e092f214c998db67475be1935
Anshul Joshi <anshuljoshi22122006@gmail.com>
2026-09-13 23:18:22 +0530

    searchinsortedmatrix

---
+++ Arrays/searchinsortedmatrix.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class searchinsortedmatrix{
+    public static boolean search(int matrix[][] , int key){
+        int row = 0;
+        int col = matrix[0].length-1;
+
+        while(row<matrix.length && col>=0){
+            if(key<matrix[row][col]){
+                col--;
+            }
+            else if(key>matrix[row][col]){
+                row++;
+            }
+            else{
+                System.out.print("( key found at : " + row + ',' + col + ')');
+                return true;
+            }
+        }
+        System.out.print("key not found");
+        return false;
+    }
+    public static void main(String args[]){
+        int matrix[][] = {{10,20,30,40},
+                        {15,25,35,45},
+                        {27,29,37,48},
+                        {32,33,39,50}} ;
+        
+        int key = 37;
+        search(matrix , key);
+    }
+}
