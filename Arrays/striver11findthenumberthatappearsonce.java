cf400f0fbd94d44c3864b9b9abb219c066325ec3
Anshul Joshi <anshuljoshi22122006@gmail.com>
2026-09-20 17:57:55 +0530

    add

--- Arrays/striver11findthenumberthatappearsonce.java
+++ Arrays/striver11findthenumberthatappearsonce.java
@@ -1,33 +1,33 @@
 // Better approach
 
-// import java.util.*;
-// public class striver11findthenumberthatappearsonce{
-//     public static int numberthatappearonce(int arr[]){
-//         int max = 0;
+import java.util.*;
+public class striver11findthenumberthatappearsonce{
+    public static int numberthatappearonce(int arr[]){
+        int max = 0;
 
-//         for(int i=0 ; i<arr.length ; i++){
-//             max = Math.max(max,arr[i]);
-//         }
+        for(int i=0 ; i<arr.length ; i++){
+            max = Math.max(max,arr[i]);
+        }
     
-//         int hasharr[] = new int[max+1];
+        int hasharr[] = new int[max+1];
 
-//         for(int i=0 ; i<arr.length ; i++){
-//             hasharr[arr[i]]++;
-//         }
+        for(int i=0 ; i<arr.length ; i++){
+            hasharr[arr[i]]++;
+        }
 
-//         for(int i=0 ; i<hasharr.length ; i++){
-//             if(hasharr[arr[i]] == 1){
-//                 return arr[i];
-//             }
-//         }
-//         return -1;
-//     }
+        for(int i=0 ; i<hasharr.length ; i++){
+            if(hasharr[arr[i]] == 1){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
 
-//     public static void main(String args[]){
-//         int arr[] = {2,1,3,1,4,5,4,6,7,6,7,2,5};
-//         System.out.print(numberthatappearonce(arr));
-//     }
-// }
+    public static void main(String args[]){
+        int arr[] = {2,1,3,1,4,5,4,6,7,6,7,2,5};
+        System.out.print(numberthatappearonce(arr));
+    }
+}
 
 
 // Optimal approach
