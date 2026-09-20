ddc0d36ec4901430eb0cf75138cb496fe7030a87
Anshul Joshi <anshuljoshi22122006@gmail.com>
2026-09-20 11:32:50 +0530

    add the problems

---
+++ Arrays/striver10maxconsceutiveones.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class striver10maxconsceutiveones{
+    public static int maxconsceutive(int arr[]){
+        int count = 0;
+        int max = 0;
+        for(int i=0 ; i<arr.length ; i++){
+            if(arr[i] == 1){
+                count++;
+                max = Math.max(count,max);
+            }
+            else{
+                count = 0;
+            }
+        }
+        return max;
+    }
+    public static void main(String args[]){
+        int arr[] = {1,1,0,1,1,1};
+        System.out.print(maxconsceutive(arr));
+    }
+}
